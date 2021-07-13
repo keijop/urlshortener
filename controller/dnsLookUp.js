c4afc7e4a34f140e9353c3b3ca1bd13d8bb8162a
keijop <76419484+keijop@users.noreply.github.com>
2021-07-13 07:12:01 +0300

    Update dnsLookUp.js

--- controller/dnsLookUp.js
+++ controller/dnsLookUp.js
@@ -2,8 +2,9 @@
 const dns = require('dns')
 
 const dnsLookUp = (req,res) => {
-	let host = req.body
 
+	let host = req.body
+	console.log(typeof req.body)
 	dns.lookup(host, (err, address) => {
 		let result = err ? { error: 'invalid url' } : { original_url : host, short_url : 1, address : address}
 		res.json(result)
