0381027e709b66b1e0e19d6fa88ec63de2e128a3
keijop <76419484+keijop@users.noreply.github.com>
2021-08-06 13:44:24 +0300

    changes

--- controller/dnsLookUp.js
+++ controller/dnsLookUp.js
@@ -1,16 +1,27 @@
 
-const dns = require('dns')
+const validUrl = require('valid-url')
 
 const dnsLookUp = (req,res) => {
 
-	let host = req.body.url
+	let uriFromForm = req.body.url
 
-  console.log(host)
+	let urlCheck = validUrl.isWebUri(uriFromForm)
 
-	dns.lookup(host, (err, address) => {
-		let result = err ? { error: 'invalid url' } : { original_url : host, short_url : 1}
-		res.json(result)
-	})
+	let result = urlCheck ? {original_url : urlCheck, short_url : 1} : { error: 'invalid url' }
+
+	res.status(200).json(result)
 }
+
+// const dnsLookUp = (req,res) => {
+
+// 	let host = req.body.url
+
+//   console.log(host)
+
+// 	dns.lookup(host, (err, addresses) => {
+// 		let result = err ? { error: 'invalid url' } : { original_url : host, short_url : 1, addresses: addresses}
+// 		res.json(result)
+// 	})
+// }
 
 module.exports = dnsLookUp
